3500663aafc67357c4e435cde804c40c9eb2e9d9
Salvatore De Simone <salvatore.desimone@powellind.com>
2019-02-15 09:14:27 -0800

    Cleaning process having each branch creating a test file in the test files folder so that main stays empty

--- 60100-sw-py-serial-devices.py
+++ 60100-sw-py-serial-devices.py
@@ -1,17 +1,4 @@
-from ThermatronDevice import Thermotron
-
-
-# test code below
-print('Thermotron driver tester')
-
-myThermatronDevice1 = Thermotron("COM5")        # this is an object
-myThermatronDevice1.openConnection()            # connection is ope
-
-myDataRead = myThermatronDevice1.readData("readSetPoint", 50)
-print("Test 1: dta recieved from thermatron setpoint one", myDataRead)
-
-
-myThermatronDevice1.closeConnection()
+#Clean main 
 
 
 
