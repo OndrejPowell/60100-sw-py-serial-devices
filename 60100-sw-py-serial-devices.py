26e9c67e116c4ac7f4f79b40c04e7a0946d97e79
“Ondrej <Ondrej.mecl@powellind.com>
2019-02-21 14:39:54 -0800

    Fixed some merging errors

--- 60100-sw-py-serial-devices.py
+++ 60100-sw-py-serial-devices.py
@@ -1,9 +1,44 @@
-<<<<<<< HEAD
 #Clean Main
-=======
-# Clean Main
 
->>>>>>> feature/change_fluke_class
+# Main application functional blocks:
+#
+# 1) initialization of application from Settings.ini file
+#    including  - serail devices
+#               - modbus devices
+#               - log file name
+# intend:
+# this file 
+#
+# this exampole application will have following functionality:
+#  - control Thermotron evironmental chamber and cycle through defined setpoints
+#  - measure reference temperaure using Fluke 1523 
+#  - collect the data from 2G3 units
+
+# to do:
+# initit the devices
+# read the devices first
+
+# import modules
+from G3Device import G3Device
+from ThermatronDevice import Thermotron
+
+
+
+
+from time import sleep
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
